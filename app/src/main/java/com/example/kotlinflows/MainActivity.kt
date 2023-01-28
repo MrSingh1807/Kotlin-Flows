985ea19db8bd418488b6189b04b404a4b422b946
Mayank Singh Shakya <mayank1807.singh@gmail.com>
2023-01-29 00:31:41 +0530

    basic implimentation of flows

--- app/src/main/java/com/example/kotlinflows/MainActivity.kt
+++ app/src/main/java/com/example/kotlinflows/MainActivity.kt
@@ -2,10 +2,51 @@ package com.example.kotlinflows
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.kotlinflows.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val TAG = "MyTaG"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startFlowBTN.setOnClickListener {
+            CoroutineScope(Dispatchers.Main).launch {
+                val data: Flow<Int> = producer()
+                /*
+                Flows by default are Cold, until consumer not present
+                 */
+
+                data.collect {
+                    // ie: Consumer; Here we collect data,
+                    Log.d(TAG, "$it , ->  ")
+
+                    binding.flowDataTV.append( "$it \n  ")
+                }
+            }
+        }
+
+    }
+
+
+    private fun producer() = flow<Int> {
+        val list = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
+        // Assume that, this is a data which flows consistently
+
+        list.forEach {
+            delay(1000)
+            emit(it)
+
+            // Here, Producer produce the for collector
+        }
+
+
     }
 }
