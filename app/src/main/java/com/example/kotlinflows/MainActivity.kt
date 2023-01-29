02602af2a7c6aaf0b09439e773cf6692d5a06c66
Mayank Singh Shakya <mayank1807.singh@gmail.com>
2023-01-30 01:32:08 +0530

    try catch

--- app/src/main/java/com/example/kotlinflows/MainActivity.kt
+++ app/src/main/java/com/example/kotlinflows/MainActivity.kt
@@ -7,6 +7,7 @@ import com.example.kotlinflows.databinding.ActivityMainBinding
 import com.example.kotlinflows.model.FormattedNote
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -20,44 +21,37 @@ class MainActivity : AppCompatActivity() {
             /********  Cold Flows   *******/
 
             CoroutineScope(Dispatchers.Main).launch {
-                /******    Context Preservation ( Flow On )  *****/
+                /******    Exception Handling  *****/
 
-                /* Flow Always preserve his context,
-                     they assume that emit() & collect() are in same context
-                Producer always produce your data on that context in which context you consume data
-
-                 Case -> I want to produce data in IO thread & consume it in Main Thread ;
-                    Use FlowON ; in multiple flowOn: flowOn always work upstreams,
-                                 code b/w ABC to XYZ flowOn is run in XYZ flowOn 
+                /*
+                There are two Specific point where error can be occur;
+                       either -> producing data
+                       or -> collecting data
                  */
 
-                producer()
-                    .map {
-                        Log.d(TAG, "Consumer map " + Thread.currentThread().name) // Default thread
-                        it * 2
-                    }
-                    .buffer(2)
-                    .flowOn(Dispatchers.Unconfined)  // Before this or b/w last to this flowOn; all code run in passed Thread Pools
-                    .filter {
-                        Log.d(TAG, "Consumer filter " + Thread.currentThread().name) // IO thread
-                        it <= 10
-                    }
-                    .flowOn(Dispatchers.IO)   // Before this or b/w last to this flowOn; all code run in passed Thread Pools
-                    .collect {
-                        Log.d(TAG, "Consumer " + Thread.currentThread().name) // Main Thread
-                        binding.flowDataTV.append("Consumer -> $it \n ")
-                    }
+                try {
+                    producer()
+                        .collect {
+                            Log.d(TAG, "Consumer " + Thread.currentThread().name) // Main Thread
+                            binding.flowDataTV.append("Consumer -> $it \n ")
+//                            throw Exception("Error in Collector")
+                        }
 
+                } catch (e: Exception){
+                    // You can easily handel the both errors in one try catch
+                    Log.d(TAG, e.message.toString())
+                }
             }
         }
     }
 
     private fun producer() = flow {
-            val list = listOf(1, 2, 3, 4, 5, 6, 7, 8)
-            list.forEach {
-                delay(1000)
-                Log.d(TAG, "Producer " + Thread.currentThread().name)
-                emit(it)
-            }
+        val list = listOf(1, 2, 3, 4, 5, 6, 7, 8)
+        list.forEach {
+            delay(1000)
+            Log.d(TAG, "Producer " + Thread.currentThread().name)
+            emit(it)
+            throw Exception("Error in Producer")
+        }
     }
 }
